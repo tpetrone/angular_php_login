0d8c132551d319628f4f2693c0f43e5f832003c6
Petrone <thiago.petrone@gmail.com>
2015-11-09 15:58:43 -0200

    Update config.php

--- api/config.php
+++ api/config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'r75jq11');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'angularcode');
 
